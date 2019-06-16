ce41b0fc209d8472794acbbe7c65b06adba8fc46
Andrew Jones <andrew@andrewjones.id.au>
2019-06-17 09:48:47 +1000

    First cut of enabling cross platform

--- go.sh
+++ go.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-set -eu
+set -u
 
-sudo apt update
-sudo apt upgrade
+version="${1:-linux}"
 
-which ansible
+if [[ `uname` == 'Darwin' ]]; then
+    which ansible
 
-if [ ! $? -eq 0 ]; then
-    sudo add-apt-repository ppa:ansible/ansible
-    sudo apt-get update
-    sudo apt-get install -y ansible
+    if [ ! $? -eq 0 ]; then
+        set -e
+
+        brew install ansible
+    fi;
+
+elif [[ `uname` == 'Linux' ]]; then
+    sudo apt update
+    sudo apt upgrade
+
+    which ansible
+
+    if [ ! $? -eq 0 ]; then
+        set -e
+
+        sudo add-apt-repository ppa:ansible/ansible
+        sudo apt-get update
+        sudo apt-get install -y ansible
+    fi;
 fi;
 
-ansible-playbook -K -i "localhost," -c local workstation.yml $@
+set -x
+
+ansible-playbook -K -i "localhost," -c local ${version}.yaml "${@:2}"
